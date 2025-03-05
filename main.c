1ed0103479c5d1313c732567ae9fcd3a7beaf8c2
Bohdan Ovsiannikov <bohdan_ovsiannikov@comcast.com>
2025-03-05 03:24:57 +0200

    Add basic listening

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+
+#include "karma.h"
+
+void karma_listener(KarmaMessage msg) {
+	fwrite(msg.payload, sizeof(uint8_t), msg.payload_size, stdout);
+}
 
 int main() {
-	printf("If a man speaks or acts with a pure thought, happiness follows him, like a shadow that never leaves him.\n");
+	Karma karma = form_karma();
+
+	karma_add_listener(&karma, 0, karma_listener);
+
+	char *payload = "If a man speaks or acts with a pure thought, happiness follows him, like a shadow that never leaves him.\n";
+	size_t len = strlen(payload);
+	KarmaMessage msg;
+	msg.payload_size = len;
+	msg.payload = payload;
+
+	karma_post_message(karma, 0, msg);
+
 	return 0;
 }
